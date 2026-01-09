adb9ea278329425a1cd618669cc1dc1fa59e04ea
rijogeorge <rijothedeveloper@gmail.com>
2026-01-08 18:11:03 -0800

    init script error resolved

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -1,7 +1,21 @@
-#!/bin/sh
+##!/bin/sh
+#set -e
+#
+#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "CREATE DATABASE products;"
+#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "CREATE DATABASE orders;"
+#
+#echo "Databases created successfully!"
+
+
+#!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "CREATE DATABASE products;"
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "CREATE DATABASE orders;"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE DATABASE products;
+    CREATE DATABASE orders;
+
+    GRANT ALL PRIVILEGES ON DATABASE products TO "$POSTGRES_USER";
+    GRANT ALL PRIVILEGES ON DATABASE orders TO "$POSTGRES_USER";
+EOSQL
 
 echo "Databases created successfully!"
